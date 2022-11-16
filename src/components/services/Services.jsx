fd47764becee4f48a86dd85bfcadc401f91741ce
boujrada yassine <63125972+yassineboujrada@users.noreply.github.com>
2022-11-16 23:38:07 +0100

    services page component

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,125 @@
 import React from 'react';
 import './services.css';
+import {BiCheck} from 'react-icons/bi';
 
 export const Services = () => {
   return (
-    <section id='services'>Services</section>
+    <section id='services'>
+        <h5>What I Offer</h5>
+        <h2>Services</h2>
+
+        <div className='container services_container'>
+            <article className='service'>
+                <div className='service__head'>
+                    <h3>UX design</h3>
+                </div>
+
+                <ul className='service__list'>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                </ul>
+            </article>
+
+            <article className='service'>
+                <div className='service__head'>
+                    <h3>FullStack Developement</h3>
+                </div>
+
+                <ul className='service__list'>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                </ul>
+            </article>
+
+            <article className='service'>
+                <div className='service__head'>
+                    <h3>Data Analyst</h3>
+                </div>
+
+                <ul className='service__list'>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                    <li>
+                        <BiCheck className='service__list-icon' />
+                        <p>Loremvicnicnvinvrivnrnnviniveinerirnrvirnvivr</p>
+                    </li>
+                </ul>
+            </article>
+        </div>
+    </section>
   )
 }
