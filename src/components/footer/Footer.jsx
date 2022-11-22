7027a42db6416c685fd31b3374a47c99c13406b8
boujrada yassine <63125972+yassineboujrada@users.noreply.github.com>
2022-11-22 23:59:28 +0100

    footer section

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
 import './footer.css'
+import {AiFillLinkedin,AiFillGithub} from 'react-icons/ai'
+import {FiInstagram} from 'react-icons/fi'
 
 export const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>Boujrada</a>
+
+      <ul className='footer__links'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experiences'>My Projects</a></li>
+        <li><a href='#contact'>Contact Me</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://www.instagram.com/yassine_boujrada/' target="_blank">
+          <FiInstagram />
+        </a>
+        <a href="https://www.linkedin.com/in/yassineboujrada" target="_blank">
+            <AiFillLinkedin />
+        </a>
+        <a href="https://github.com/yassineboujrada" target="_blank">
+            <AiFillGithub />
+        </a>
+      </div>
+
+      <div className='footer__copyright'>
+        <small>&copy; Boujrada Yassine All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
